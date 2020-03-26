1779886b18b6fab19f07bf8a582e27f02d96699c
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-26 13:50:06 -0700

    Fixing few failing tests for prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,16 +1,35 @@
 var Queue = function(name) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = Object.create(Queue.prototype);
+  var someInstance = Object.create(queueMethods);
 
-  someInstance.name = name;
+  someInstance.storage = {};
 
   return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    var sizeVar = this.size();
+    this.storage[sizeVar] = value;
+  },
+  dequeue: function() {
+    var initialDequedValue = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    var sizeVar1 = Object.keys(this.storage).length;
+    delete this.storage[sizeVar1 - 1];
+    return initialDequedValue;
+  },
+  size: function() {
+    var result = Object.keys(this.storage);
+    return result.length;
+  }
+};
 
-Queue.prototype.enqueue = function(name) {
+/* Queue.prototype.enqueue = function(name) {
   this.name = name;
   var sizeVar = Object.keys(queueMethods).length;
   queueMethods[sizeVar] = this.name;
@@ -30,6 +49,6 @@ Queue.prototype.dequeue = function() {
 Queue.prototype.size = function() {
   var result = Object.keys(queueMethods);
   return result.length;
-};
+}; */
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,7 +1,7 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = Object.create(Stack.stuff);
+  var someInstance = Object.create(stackMethods);
 
   //this.name = name;
   someInstance.storage = {};
@@ -9,24 +9,26 @@ var Stack = function() {
   return someInstance;
 };
 
-Stack.stuff = {};
-
-Stack.stuff.push = function(value) {
-  var sizeVar = this.size().length;
-  this.storage[sizeVar] = value;
+var stackMethods = {
+  push: function(value) {
+    var sizeVar = this.size();
+    this.storage[sizeVar] = value;
+  },
+  pop: function() {
+    var sizeVar1 = this.size();
+    var value = this.storage[sizeVar1 - 1];
+    delete this.storage[sizeVar1 - 1];
+    return value;
+  },
+  size: function() {
+    var result = Object.keys(this.storage);
+    return result.length;
+  }
 };
 
-Stack.stuff.pop = function() {
-  var sizeVar1 = this.size().length;
-  var value = this.storage[sizeVar1 - 1];
-  delete this.storage[sizeVar1 - 1];
-  return value;
-};
-
-Stack.stuff.size = function() {
-  var result = Object.keys(this.storage);
-  return result.length;
-};
+var stack1 = Stack();
+stack1.push('priya');
+console.log(stack1.size());
 
 
 
