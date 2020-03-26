24b79842d28971653e0f8063c5a2d8f4393d8aca
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-26 00:10:40 -0700

    Stack and Queue functional-shared implementation

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,45 @@
-var Queue = function() {
+var Queue = function(name) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.name = name;
+
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+
+  return someInstance;
 };
 
 var queueMethods = {};
+var storage = {};
 
+queueMethods.enqueue = function(name) {
+  this.name = name;
+  var sizeVar = Object.keys(storage).length;
+  storage[sizeVar] = this.name;
+};
+
+queueMethods.dequeue = function() {
+  var initialDequedValue = storage[0];
+  delete storage[0];
+  for (var key in storage) {
+    storage[key - 1] = storage[key];
+  }
+  var sizeVar1 = Object.keys(storage).length;
+  delete storage[sizeVar1 - 1];
+  return initialDequedValue;
+};
+
+queueMethods.size = function() {
+  var result = Object.keys(storage);
+  return result.length;
+};
+
+var queue1 = Queue();
+queue1.enqueue('a');
+queue1.enqueue('b');
+queue1.dequeue();
+queue1.enqueue('c');
+console.log(queue1.dequeue()); //b
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,59 @@
-var Stack = function() {
+/* Functional instantiation with shared methods: same as step 1, but with shared methods
+
+Do:
+Work within the src/functional-shared/ folder
+Create an object that holds the methods that will be shared by all instances of the class
+Use the keyword this in your methods
+Use _.extend  to copy the methods onto the instance
+Don't:
+Use the keyword new or prototype chains
+Example: functional instantiation example  */
+
+var Stack = function(name) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.name = name;
+
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+
+  return someInstance;
 };
 
 var stackMethods = {};
+var storage = {};
+stackMethods.push = function(name) {
+  this.name = name;
+  var sizeVar = Object.keys(storage).length;
+  storage[sizeVar] = this.name;
+};
+
+stackMethods.pop = function() {
+  var sizeVar1 = Object.keys(storage).length;
+  var value = storage[sizeVar1 - 1];
+  delete storage[sizeVar1 - 1];
+  return value;
+};
+
+stackMethods.size = function() {
+  var result = Object.keys(storage);
+  return result.length;
+};
+
+// var stack1 = Stack();
+// stack1.push('priya');
+// stack1.pop();
+// stack1.pop();
+//stack1.push('koneru');
+//stack1.pop();
+//console.log(stack1.size());
+
+// var stack1 = Stack();
+// stack1.push('a');
+// stack1.push('b');
+// stack1.pop();
+// console.log(stack1.size());
 
 
