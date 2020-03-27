d2147151f7d56066f64068a3626b5e3b26a715a1
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-26 18:23:36 -0700

    prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,27 +3,28 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = Object.create(stackMethods);
 
-  //this.name = name;
   someInstance.storage = {};
 
   return someInstance;
 };
+//Stack.prototype.constructor = Stack;
+//Stack.prototype = Object.create(Stack.prototype);
 
-var stackMethods = {
-  push: function(value) {
-    var sizeVar = this.size();
-    this.storage[sizeVar] = value;
-  },
-  pop: function() {
-    var sizeVar1 = this.size();
-    var value = this.storage[sizeVar1 - 1];
-    delete this.storage[sizeVar1 - 1];
-    return value;
-  },
-  size: function() {
-    var result = Object.keys(this.storage);
-    return result.length;
-  }
+stackMethods = {};
+
+stackMethods.push = function(value) {
+  var sizeVar = this.size();
+  this.storage[sizeVar] = value;
+},
+stackMethods.pop = function() {
+  var sizeVar1 = this.size();
+  var value = this.storage[sizeVar1 - 1];
+  delete this.storage[sizeVar1 - 1];
+  return value;
+},
+stackMethods.size = function() {
+  var result = Object.keys(this.storage);
+  return result.length;
 };
 
 var stack1 = Stack();
