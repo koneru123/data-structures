fd0afc0619c7bad7ca8d524fcc6e1927b4a1d4a9
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-27 23:12:33 -0700

    linked list and tree data structure implementation

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,62 @@
 var LinkedList = function() {
   var list = {};
+  var storage = {};
   list.head = null;
   list.tail = null;
+  //storage.head = null;
+  //storage.tail = null;
+
+  //var storage = {};
 
   list.addToTail = function(value) {
+    // if it is the first value in the list, it will be both the head and tail
+    // Time complexity: constant time
+    //Node(value);
+    // creating a node instance
+    var node1 = Node(value);
+    // if the head is null then add node1 as head and tail
+    if (list.head === null) {
+      list.head = node1;
+      list.tail = list.head;
+      node1.next = null;
+      // if head is not null, then make node1 as tail
+    } else {
+      list.tail = node1;
+      list.head.next = value;
+    }
   };
 
   list.removeHead = function() {
+    // if we remove head, the next value becomes the head
+    // if you remove the last value in the list, head and tail become null
+    // Time complexity: constant time
+    // delete list.head;
+    //list.head = list.head
+    var oldHeadValue = list.head.value;
+    if (list.head.next === null) {
+      list.head = null;
+      list.tail = null;
+    } else {
+      list.head.next = list.tail.next;
+      list.head.value = list.tail.value;
+    }
+    return oldHeadValue;
   };
 
   list.contains = function(target) {
+    // Will return true if node exists in the list
+    // Will return false if it does not exist in the list
+    // Time complexity: linear
+    for (var key in list) {
+      var response = false;
+      if (list[key].value !== target) {
+        continue;
+      } else if (list[key].value === target) {
+        response = true;
+        break;
+      }
+    }
+    return response;
   };
 
   return list;
@@ -24,6 +71,21 @@ var Node = function(value) {
   return node;
 };
 
+var linkedlist1 = LinkedList();
+linkedlist1.addToTail(4);
+linkedlist1.addToTail(5);
+linkedlist1.removeHead();
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// How to create a node
+
+// if node is the last one, it will have a tail
+
+// if node is the first one, it will have a head
+
+// if node if the middle one, remove head, remove tail, add pointer to next node and the preceeding node will have pointer to the current node.
+
+// when finding a node, we need to return true if node is existing in the list, if not return false
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,13 +14,39 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // Sets the value as target of node
+  // Adds the node as child of the tree
+  const len = this.children.length;
+  if (len === 0) {
+    this.children[0] = Tree(value);
+  } else {
+    this.children[len] = Tree(value);
+  }
 };
 
 treeMethods.contains = function(target) {
+  // Time complexity: linear;
+  //console.log('start' + this.value + ' target=' + target);
+  if (this.value === target) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+    return false;
+  }
 };
 
+//tree.children = [{value: 5, children: []}]
 
+//tree.children = [{value: 5, children: [{value: 22, children: []}]}, {value: 10, children: []}];
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+var tree = Tree();
+tree.addChild(5);
+tree.addChild(6);
+tree.children[0].addChild(7);
+tree.children[1].addChild(8);
+console.log(tree.contains(7)); //true
+console.log(tree.contains(8)); //true
